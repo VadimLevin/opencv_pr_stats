b3b90eec1753d2de00ffd2b7f55c74a2c84adb39
Vadim Levin <vadim.levin@xperience.ai>
2019-12-18 15:55:38 +0300

    Add github target to Makefile. Fix typo

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = u'OpenCV pull requests statistic'
+project = u'OpenCV pull requests statistics'
 copyright = u'2019, opencv'
 author = u'opencv'
 
